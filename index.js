e1386672a4b675f76f7f0119b30f2e1eabbec86f
Quintia <quintialy@gmail.com>
2021-07-24 21:47:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+writeCards(["Lisa","Kaitlin","Jan"], "birthday");
+const event = 'gift';
+
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++) { 
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+
+    }
+    return newArr;
+}
+
+function countDown(positiveNumber) {
+    while(positiveNumber >= 0) {
+      console.log(positiveNumber);
+      positiveNumber--;  
+    }
+}
